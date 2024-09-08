5f542fbff7a0e6568083743b090b48ebcc082e3d
Cristian <cdom127@wgu.edu>
2024-09-08 16:41:47 -0700

    Temporary pages for app

---
+++ app/color-palette-generator/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColorPaletteGeneratorPage = () => {
+  return <div>ColorPaletteGeneratorPage</div>;
+};
+
+export default ColorPaletteGeneratorPage;
---
+++ app/palette/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PaletteDetailsPage = () => {
+  return <div>PaletteDetailsPage</div>;
+};
+
+export default PaletteDetailsPage;
