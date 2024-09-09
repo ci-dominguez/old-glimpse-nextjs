4cb1551e05028631fb04922c137cfb244be390c4
ci-dominguez <c.isaacdominguez@gmail.com>
2024-09-08 23:16:16 -0700

    Moved generator out of the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-"use client";
-import { useState } from "react";
-import ColorPaletteGenerator from "@/components/ColorPaletteGenerator";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
@@ -10,22 +9,55 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from '@/components/ui/dialog';
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <main className='flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='text-3xl font-bold mb-4'>
-        AI Color Palette Generator 🤖🧑‍🎨
-      </h1>
-      <ColorPaletteGenerator />
+      <h1 className='text-3xl font-bold text-center'>Glimpse ✨</h1>
+
+      <p className='text-sm text-muted-foreground text-center mb-4'>
+        A toolkit to <i className='text-foreground'>design</i> your design
+        system
+      </p>
+      <div className='flex space-x-4'>
+        <Button type='button'>
+          <Link href='/sign-up'>Get Started</Link>
+        </Button>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+          <DialogTrigger asChild>
+            <Button variant='outline'>View Example</Button>
+          </DialogTrigger>
+          <DialogContent className='sm:max-w-[600px]'>
+            <DialogHeader>
+              <DialogTitle>
+                How to Use the AI Color Palette Generator
+              </DialogTitle>
+              <DialogDescription>
+                Watch this short video to learn how to use the generator
+                effectively.
+              </DialogDescription>
+            </DialogHeader>
+            <div className='aspect-video'>
+              <iframe
+                width='100%'
+                height='100%'
+                src='https://www.youtube.com/embed/watch?v=0nF69UTw99E&list=RD0nF69UTw99E'
+                title='AI Color Palette Generator Tutorial'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
 
       <h2 className='text-violet-400 font-bold pt-10'>
-        Made by{" "}
+        Made by{' '}
         <Link
-          href='https://cidominguez.com'
+          href='https://glimpse.cidominguez.com'
           className='underline'
           target='_blank'
           rel='noopener noreferrer'
@@ -34,7 +66,7 @@ export default function Home() {
         </Link>
       </h2>
       <h2 className='text-blue-400 font-bold'>
-        Repo{" "}
+        Repo{' '}
         <Link
           href='https://github.com/ci-dominguez/ai-color-palette-nextjs-ts'
           className='underline'
@@ -44,32 +76,6 @@ export default function Home() {
           here
         </Link>
       </h2>
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogTrigger asChild>
-          <Button variant='outline' className='mt-6'>
-            View Example
-          </Button>
-        </DialogTrigger>
-        <DialogContent className='sm:max-w-[600px]'>
-          <DialogHeader>
-            <DialogTitle>How to Use the AI Color Palette Generator</DialogTitle>
-            <DialogDescription>
-              Watch this short video to learn how to use the generator
-              effectively.
-            </DialogDescription>
-          </DialogHeader>
-          <div className='aspect-video'>
-            <iframe
-              width='100%'
-              height='100%'
-              src='https://www.youtube.com/embed/watch?v=0nF69UTw99E&list=RD0nF69UTw99E'
-              title='AI Color Palette Generator Tutorial'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              allowFullScreen
-            />
-          </div>
-        </DialogContent>
-      </Dialog>
     </main>
   );
 }
