c9d260010e8237023eafdd1bf6062fbad54ae8ab
ci-dominguez <c.isaacdominguez@gmail.com>
2024-09-09 13:46:22 -0700

    Update config to align with expected raw data instead of parsing during webhook

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/webhooks/clerk',
+        headers: [{ key: 'Content-Type', value: 'application/json' }],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
