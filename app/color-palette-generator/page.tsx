95e2e32368f415fd95d30179cfd05fb6cf9b7dbb
ci-dominguez <c.isaacdominguez@gmail.com>
2024-09-08 23:24:18 -0700

    Add color palette generator to its own page

--- app/color-palette-generator/page.tsx
+++ app/color-palette-generator/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+// import {Auth} from '@clerk/nextjs'
+import ColorPaletteGenerator from '@/components/ColorPaletteGenerator';
 
 const ColorPaletteGeneratorPage = () => {
-  return <div>ColorPaletteGeneratorPage</div>;
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <ColorPaletteGenerator />
+    </main>
+  );
 };
 
 export default ColorPaletteGeneratorPage;
