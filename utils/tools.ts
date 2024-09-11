7f7bb71d2569c75de061e0d133649cf368045175
Cristian <cdom127@wgu.edu>
2024-09-11 16:43:35 -0700

    Add text and svg assets

---
+++ utils/tools.ts
@@ -0,0 +1,132 @@
+export const tools = [
+  {
+    category: 'Glimpse AI',
+    tools: [
+      {
+        name: 'Color Systems',
+        description:
+          "Our AI-powered color system generator that creates comprehensive palettes based on your brand/app's essence.",
+        link: '/features/ai/color-systems',
+        limited: true,
+      },
+      {
+        name: 'Mood Matcher',
+        description:
+          "Feeling blue? Or maybe sunny and bright? Tell us your mood, and we'll mix the perfect colors to match your vibes!",
+        link: '/features/ai/mood-matcher',
+        limited: true,
+      },
+      {
+        name: 'Co-Generation',
+        description:
+          'Give us a few key colors, and we will produce an in-depth analysis of your colors alongside our generated suggestions.',
+        link: '/features/ai/co-generation',
+        limited: true,
+      },
+    ],
+  },
+  {
+    category: 'Realtime Collaboration',
+    tools: [
+      {
+        name: 'Palette Editor',
+        description:
+          'A color playground where you can mix, match, and share your colors with your team.',
+        link: '/features/collab/palette-editor',
+        limited: true,
+      },
+      {
+        name: 'Gradient Designer',
+        description: 'Craft gradients with our intuitive gradient visualizer.',
+        link: '/features/collab/gradient-designer',
+        limited: true,
+      },
+    ],
+  },
+  {
+    category: 'Libraries',
+    tools: [
+      {
+        name: 'Color Swatches',
+        description: 'Browse through our curated collection of colors.',
+        link: '/features/libraries/swatch-curation',
+        limited: true,
+      },
+      {
+        name: 'Color Books',
+        description: 'A comprehensive catalog of color books for inspiration.',
+        link: '/features/libraries/color-books',
+        limited: true,
+      },
+      {
+        name: 'Palette Gallery',
+        description: 'Get inspired by our museum of color palettes.',
+        link: '/features/libraries/palette-gallery',
+        limited: false,
+      },
+      {
+        name: 'Gradient Gallery',
+        description: 'Find ready-to-use gradients for your next app.',
+        link: '/features/libraries/gradient-gallery',
+        limited: false,
+      },
+      {
+        name: 'Color Trends',
+        description: 'Travel through time with our color trend archive.',
+        link: '/features/libraries/trend-archive',
+        limited: true,
+      },
+      {
+        name: 'Brand Color Vault',
+        description:
+          "Get inspired by the colors of the world's most iconic brands.",
+        link: '/features/libraries/brand-database',
+        limited: true,
+      },
+    ],
+  },
+  {
+    category: 'Accessibility Suite',
+    tools: [
+      {
+        name: 'Contract Checker',
+        description:
+          'Ensure your colors pass accessibility standards with flying colors.',
+        link: '/features/accessibility/contrast-checker',
+        limited: false,
+      },
+      {
+        name: 'Visual Impairment Simulator',
+        description:
+          'Inclusive design is not just black and white, simulate various visual impairments to see how your designs fare.',
+        link: '/features/accessibility/impairment-simulator',
+        limited: true,
+      },
+    ],
+  },
+  {
+    category: 'Exports & Integrations',
+    tools: [
+      {
+        name: 'Color Spaces',
+        description:
+          'We support a wide range of color spaces including but not limited to rgb, hsl, cmyk, and lab.',
+        link: '/features/exporting/color-spaces',
+        limited: true,
+      },
+      {
+        name: 'Exporting',
+        description:
+          'Export your designs in various formats like CSS/SCSS variables, TailwindCSS configs, PDF, XML, CSV, ASE, and more',
+        link: '/features/exporting/code-generator',
+        limited: true,
+      },
+      {
+        name: 'Palette Extractor',
+        description: 'Pull color palettes from any image.',
+        link: '/features/exporting/image-to-palette',
+        limited: true,
+      },
+    ],
+  },
+];
