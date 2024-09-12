eeee1bdae06065fef8a121bced1516f9d351420b
Cristian <cdom127@wgu.edu>
2024-09-11 20:51:04 -0700

    Add image source config from clerk

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
