e0fd583b88dc79c6594b7caca5582853b1a37009
ci-dominguez <c.isaacdominguez@gmail.com>
2024-09-11 23:28:40 -0700

    Map new links arrays in the footer

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,14 +1,14 @@
 'use client';
 import { useNav } from '@/contexts/NavContext';
-import { resourceLinks as links } from '@/utils/links';
 import Link from 'next/link';
 import Logo from '../icons/logo';
 import { Button } from '../ui/button';
+import { toolLinks, resourceLinks } from '@/utils/links';
 
 const Footer = () => {
   const { setExtended } = useNav();
   return (
-    <footer className='flex flex-col m-2 px-6 py-12 space-y-6 rounded-xl bg-black text-white text-center'>
+    <footer className='flex flex-col m-2 px-6 py-12 space-y-12 rounded-xl bg-black text-white text-center'>
       <Link
         onClick={() => {
           setExtended(false);
@@ -19,32 +19,62 @@ const Footer = () => {
         <Logo className='size-8 fill-white' />
         <h1>Glimpse</h1>
       </Link>
-      <ul className='flex flex-col space-y-2'>
-        <li>
-          <h2 className='font-bold'>Resources</h2>
-        </li>
-        {links.map((link) => {
+      <div className='grid grid-cols-2 gap-x-2 gap-y-6 text-left'>
+        {toolLinks.map((link) => {
           return (
-            <li key={link.name} className='font-medium text-on'>
-              <Link href={link.href}>{link.name}</Link>
-            </li>
+            <ul key={link.categoryName}>
+              <li>{link.categoryName}</li>
+              {link.tools.map((tool) => {
+                return (
+                  <li key={tool.name} className='font-medium text-on ml-2'>
+                    <Link
+                      onClick={() => {
+                        setExtended(false);
+                      }}
+                      href={tool.href}
+                    >
+                      {tool.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
           );
         })}
-      </ul>
-      <h2 className='font-bold text-left'>
-        What if my creative process was more efficient?
-      </h2>
-      <Button className='p-0 text-md' variant='secondary'>
-        <Link
-          onClick={() => {
-            setExtended(false);
-          }}
-          href='/sign-up'
-          className='px-4 py-2 w-full'
-        >
-          It is &mdash; discover how
-        </Link>
-      </Button>
+        <ul>
+          <li>Resources</li>
+          {resourceLinks.map((link) => {
+            return (
+              <li key={link.name} className='font-medium text-on ml-2'>
+                <Link
+                  onClick={() => {
+                    setExtended(false);
+                  }}
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='flex flex-col space-y-2'>
+        <h2 className='font-bold text-left'>
+          What if my creative process was more efficient?
+        </h2>
+        <Button className='p-0 text-md' variant='secondary'>
+          <Link
+            onClick={() => {
+              setExtended(false);
+            }}
+            href='/sign-up'
+            className='px-4 py-2 w-full'
+          >
+            It can be &mdash; discover how
+          </Link>
+        </Button>
+      </div>
     </footer>
   );
 };
