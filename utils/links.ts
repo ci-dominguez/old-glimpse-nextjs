418100883f9cc10a911d8882e1c41352c699cc3b
Cristian <cdom127@wgu.edu>
2024-09-11 17:49:30 -0700

    Add tool links

--- utils/links.ts
+++ utils/links.ts
@@ -1,6 +1,56 @@
-export const links = [
+export const resourceLinks = [
   { name: 'Features', href: '/features' },
   { name: 'Pricing', href: '/pricing' },
   { name: 'About Us', href: '/about' },
   { name: 'Contact', href: '/contact' },
+];
+
+export const toolLinks = [
+  {
+    categoryName: 'Popular Tools',
+    tools: [
+      { name: 'Palette Editor', href: '/palette-editor' },
+      { name: 'Gradient Designer', href: '/gradient-designer' },
+      { name: 'Palette Gallery', href: '/palette-gallery' },
+      {
+        name: 'Gradient Gallery',
+        href: 'gradient-gallery',
+      },
+      { name: 'Color Swatches', href: '/color-swatches' },
+      {
+        name: 'Palette Extractor',
+        href: '/features/exporting/image-to-palette',
+      },
+    ],
+  },
+  {
+    categoryName: 'Advanced Tools',
+    tools: [
+      { name: 'Color Systems', href: '/color-system-generator' },
+      { name: 'Mood Matcher', href: '/mood-palettes' },
+      { name: 'Co-Generator', href: '/cogen' },
+    ],
+  },
+  {
+    categoryName: 'Libraries',
+    tools: [
+      { name: 'Color Swatches', href: '/color-swatches' },
+      { name: 'Color Books', href: '/color-books' },
+      { name: 'Color Trends', href: '/trend-archive' },
+      { name: 'Brand Color Vault', href: '/brand-database' },
+    ],
+  },
+  {
+    categoryName: 'Accessibility Tools',
+    tools: [
+      {
+        name: 'Contract Checker',
+        href: '/contrast-checker',
+      },
+      {
+        name: 'Visual Impairment Simulator',
+        href: '/impairment-simulator',
+      },
+    ],
+  },
 ];
