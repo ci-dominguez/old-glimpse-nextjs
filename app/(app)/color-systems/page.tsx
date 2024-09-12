c36681604ed9e0c42a8063f713091af30f171f14
ci-dominguez <c.isaacdominguez@gmail.com>
2024-09-12 01:09:48 -0700

    Begin display of all color systems a user has

--- app/(app)/color-systems/page.tsx
+++ app/(app)/color-systems/page.tsx
@@ -1,5 +1,17 @@
 const ColorSystemsPage = () => {
-  return <div>ColorSystemsPage</div>;
+  return (
+    <main className='flex flex-col min-h-screen'>
+      <section className='flex flex-col m-2 px-6 pt-40 pb-12 space-y-24 rounded-xl bg-sky-blue-to-soft-peach'>
+        <div className='flex flex-col space-y-6 text-center'>
+          <h1 className='text-4xl font-bold'>Your color systems</h1>
+          <p className='text-md font-medium'>
+            A color system is the heart of your brand, with each color
+            representing a world of possibilities for your idea.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default ColorSystemsPage;
