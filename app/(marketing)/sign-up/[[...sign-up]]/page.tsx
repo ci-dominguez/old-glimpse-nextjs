3e6193fd6c9e229d10c22db08d62ce8e570eb881
Cristian <cdom127@wgu.edu>
2024-09-16 13:54:06 -0700

    Update sign in page with post sign up flow for users in the subscription flow. If not just follow normal redirect.

--- app/(marketing)/sign-up/[[...sign-up]]/page.tsx
+++ app/(marketing)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,35 @@
+'use client';
 import { SignUp } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
 
 const SignUpPage = () => {
+  const [redirectUrl, setRedirectUrl] = useState('/profile');
+
+  useEffect(() => {
+    const storedRedirect = localStorage.getItem('signUpRedirect');
+
+    if (storedRedirect) {
+      setRedirectUrl(storedRedirect);
+    } else {
+      const urlParams = new URLSearchParams(window.location.search);
+      const paramRedirect = urlParams.get('redirect');
+
+      if (paramRedirect) {
+        localStorage.setItem('signUpRedirect', paramRedirect);
+        setRedirectUrl(paramRedirect);
+      }
+    }
+    console.log('Redirect URL:', redirectUrl);
+  }, [redirectUrl]);
+
   return (
     <div className='flex justify-center'>
-      <SignUp />
+      <SignUp
+        path='/sign-up'
+        routing='path'
+        signInUrl='/sign-in'
+        forceRedirectUrl={redirectUrl}
+      />
     </div>
   );
 };
