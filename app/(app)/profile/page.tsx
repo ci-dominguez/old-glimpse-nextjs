83720a7d3dbf7537ae4c41eee41df1350b88923f
Cristian <cdom127@wgu.edu>
2024-09-16 13:52:49 -0700

    Temp profile page for redirects

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -1,5 +1,11 @@
+import { SignOutButton } from '@clerk/nextjs';
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className='h-screen justify-center items-center flex'>
+      <h1>ProfilePage 🙂</h1> <SignOutButton />
+    </div>
+  );
 };
 
 export default ProfilePage;
