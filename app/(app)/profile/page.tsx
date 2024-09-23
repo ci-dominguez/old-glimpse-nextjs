a28a0396ac5cda98d78083f48258d13ace29866a
Cristian <cdom127@wgu.edu>
2024-09-23 13:37:24 -0700

    Basic profile page with sign out button

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -1,10 +1,41 @@
-import { SignOutButton } from '@clerk/nextjs';
+'use client';
+import { useUser, SignOutButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const ProfilePage = () => {
-  return (
-    <div className='h-screen justify-center items-center flex'>
-      <h1>ProfilePage 🙂</h1> <SignOutButton />
-    </div>
+  const { user } = useUser();
+
+  return user ? (
+    <main className='flex flex-col min-h-screen'>
+      <section className='flex flex-col m-2 px-6 pt-40 pb-12 space-y-5 rounded-xl text-center items-center bg-off'>
+        <Image
+          src={user.imageUrl}
+          alt={`${user.firstName}'s profile`}
+          width='100'
+          height='100'
+          className='rounded-full'
+        />
+        <h1 className='text-4xl font-bold'>{user?.firstName}</h1>
+      </section>
+
+      <section className='flex flex-col m-2 px-6 py-12 space-y-6 rounded-xl bg-off text-center'>
+        <h3 className='text-xl font-bold text-on underline'>
+          <Link href='/color-systems'>View Your color systems</Link>
+        </h3>
+      </section>
+
+      <section className='flex flex-col m-2 px-6 py-12 space-y-6 rounded-xl bg-off text-white text-center'>
+        <SignOutButton>
+          <span className='px-4 py-2 w-full text-md self-start mx-auto bg-red-600 font-bold rounded cursor-pointer'>
+            {' '}
+            Sign Out
+          </span>
+        </SignOutButton>
+      </section>
+    </main>
+  ) : (
+    <>User not found...</>
   );
 };
 
