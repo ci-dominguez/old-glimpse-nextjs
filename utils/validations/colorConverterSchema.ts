c5c4a3c3cae80108d87f802a52ad2b54a5522fa2
Cristian <cdom127@wgu.edu>
2024-10-13 19:17:22 -0700

    Remove unused files

--- utils/validations/colorConverterSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const colorConverterSchema = z.object({
-  hex_code: z.string().regex(/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, {
-    message: "Invalid hex code. Please use format '#RRGGBB' or '#RGB'",
-  }),
-});
