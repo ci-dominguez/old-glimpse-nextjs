4b72d7672b66b622ae2b6e169e5471496f28e424
Cristian <cdom127@wgu.edu>
2024-10-16 13:41:24 -0700

    Create PATCH request in api to allow users to update their color systems

--- app/api/color-systems/[id]/route.ts
+++ app/api/color-systems/[id]/route.ts
@@ -3,6 +3,8 @@ import { getAuth } from '@clerk/nextjs/server';
 import { db } from '@/db/db';
 import { users, colorSystems } from '@/db/schema';
 import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+import { editColorSystemSchema } from '@/utils/validations/editColorSystemSchema';
 
 export async function GET(
   req: NextRequest,
@@ -69,6 +71,69 @@ export async function GET(
     return NextResponse.json(colorSystem);
   } catch (error) {
     console.error('Error processing color system request:', error);
+    return NextResponse.json(
+      { error: 'An error occurred while processing your request' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { userId: clerkUserId } = getAuth(req);
+  const { id: colorSystemId } = params;
+
+  try {
+    const body = await req.json();
+    const validatedData = editColorSystemSchema.parse(body);
+
+    if (!clerkUserId) {
+      return NextResponse.json(
+        { error: 'Authentication required' },
+        { status: 401 }
+      );
+    }
+
+    const [user] = await db
+      .select({ id: users.id })
+      .from(users)
+      .where(eq(users.clerkId, clerkUserId));
+
+    if (!user) {
+      return NextResponse.json(
+        { error: 'User not found in the system' },
+        { status: 500 }
+      );
+    }
+
+    //Update color system
+    const [updatedColorSystem] = await db
+      .update(colorSystems)
+      .set({
+        name: validatedData.name,
+        updatedAt: new Date(),
+      })
+      .where(eq(colorSystems.id, colorSystemId))
+      .returning();
+
+    if (!updatedColorSystem) {
+      return NextResponse.json(
+        {
+          error:
+            'Color system not found or you do not have permission to edit it',
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(updatedColorSystem);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ error: error.errors }, { status: 400 });
+    }
+    console.error('Error updating color system:', error);
     return NextResponse.json(
       { error: 'An error occurred while processing your request' },
       { status: 500 }
