24557171fcb696b31d9ee60efbeffe3bff7e3a27
Cristian <cdom127@wgu.edu>
2024-10-16 13:41:00 -0700

    Update fetching function

--- components/displays/ColorSystemDisplay.tsx
+++ components/displays/ColorSystemDisplay.tsx
@@ -14,7 +14,18 @@ import {
 } from '@/utils/types/interfaces';
 import { Eye, Heart, PencilIcon, Share2 } from 'lucide-react';
 import { Button } from '../ui/button';
-import ColorScale from './ColorScale';
+import ColorScaleDisplay from './ColorScaleDisplay';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
+import { Tabs, TabsList, TabsTrigger } from '../ui/tabs';
+import { TabsContent } from '@radix-ui/react-tabs';
+import EditColorSystemForm from '../forms/EditColorSystemForm';
+import { useRouter } from 'next/navigation';
 
 interface ColorSystemDisplayProps {
   colorSystemId: string;
@@ -37,6 +48,7 @@ const colorSpaces = [
 ];
 
 const ColorSystemDisplay = ({ colorSystemId }: ColorSystemDisplayProps) => {
+  const router = useRouter();
   const [colorSystem, setColorSystem] = useState<ColorSystem | null>(null);
   const [colors, setColors] = useState<Color[]>([]);
   const [loading, setLoading] = useState(true);
@@ -44,59 +56,61 @@ const ColorSystemDisplay = ({ colorSystemId }: ColorSystemDisplayProps) => {
   const [selectedColorSpace, setSelectedColorSpace] =
     useState<ColorSpaceKey>('hex');
 
-  useEffect(() => {
-    async function fetchColorSystemAndColors() {
-      try {
-        //Fetch color system info
-        const colorSysResp = await fetch(
-          `/api/color-systems/${colorSystemId}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
+  const fetchColorSystemAndColors = async () => {
+    try {
+      setLoading(true);
+      //Fetch color system info
+      const colorSysResp = await fetch(`/api/color-systems/${colorSystemId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-        if (!colorSysResp.ok) {
-          const errorData = await colorSysResp.json();
-          throw new Error(errorData.error || 'Failed to fetch color system');
-        }
-
-        const colorSysData: ColorSystem = await colorSysResp.json();
-        setColorSystem(colorSysData);
-
-        // Fetch color details
-        const colorIds = [
-          ...colorSysData.baseColors,
-          colorSysData.backgroundColor,
-        ];
-        const colorResponse = await fetch(
-          `/api/colors?ids=${colorIds.join(',')}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-
-        if (!colorResponse.ok) {
-          const errorData = await colorResponse.json();
-          throw new Error(errorData.error || 'Failed to fetch colors');
-        }
-
-        const colorData: Color[] = await colorResponse.json();
-        setColors(colorData);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'Error fetching data');
-      } finally {
-        setLoading(false);
+      if (!colorSysResp.ok) {
+        const errorData = await colorSysResp.json();
+        throw new Error(errorData.error || 'Failed to fetch color system');
       }
-    }
 
+      const colorSysData: ColorSystem = await colorSysResp.json();
+      setColorSystem(colorSysData);
+
+      // Fetch color details
+      const colorIds = [
+        ...colorSysData.baseColors,
+        colorSysData.backgroundColor,
+      ];
+      const colorResponse = await fetch(
+        `/api/colors?ids=${colorIds.join(',')}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!colorResponse.ok) {
+        const errorData = await colorResponse.json();
+        throw new Error(errorData.error || 'Failed to fetch colors');
+      }
+
+      const colorData: Color[] = await colorResponse.json();
+      setColors(colorData);
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'Error fetching data');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchColorSystemAndColors();
   }, [colorSystemId]);
+
+  const handleUpdateSuccess = async () => {
+    await fetchColorSystemAndColors();
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -135,21 +149,118 @@ const ColorSystemDisplay = ({ colorSystemId }: ColorSystemDisplayProps) => {
             </SelectContent>
           </Select>
         </div>
-        <Button variant='outline' className='self-start'>
-          <Share2 className='size-4' />
-        </Button>
-        <Button variant='outline' className='self-start'>
-          <Eye className='size-4' />
-        </Button>
-        <Button variant='outline' className='self-start'>
-          <Heart className='size-4' />
-        </Button>
-        <Button variant='outline' className='self-start'>
-          <PencilIcon className='size-4' />
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant='outline' className='self-start'>
+              <Share2 className='size-4' />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className='flex flex-col'>
+            <DialogHeader>
+              <DialogTitle>Export Base Palette</DialogTitle>
+            </DialogHeader>
+            <Tabs defaultValue='css'>
+              <TabsList>
+                <TabsTrigger value='css'>Css</TabsTrigger>
+                <TabsTrigger value='code'>Code</TabsTrigger>
+              </TabsList>
+              <TabsContent value='css'>
+                <pre className='code-block p-4 rounded-lg bg-gray-100 text-primary overflow-auto max-h-[300px] text-sm'>
+                  <code>
+                    <span className='text-gray-500'>
+                      {'/* CSS HEX */'}
+                      <br />
+                    </span>
+                    {colors.map((color) => (
+                      <span key={color.id}>
+                        --{color.name.replace(/ /g, '-').toLowerCase()}:{' '}
+                        <span className='text-[#005cc5] font-bold'>
+                          {color.hex}
+                        </span>
+                        ;
+                        <br />
+                      </span>
+                    ))}
+                    <span className='text-gray-500'>
+                      <br />
+                      {'/* SCSS HEX */'}
+                      <br />
+                    </span>
+                    {colors.map((color) => (
+                      <span key={color.id}>
+                        <span className='text-[#df5000] font-bold'>
+                          ${color.name.replace(/ /g, '-').toLowerCase()}
+                        </span>
+                        :{' '}
+                        <span className='text-[#005cc5] font-bold'>
+                          {color.hex}
+                        </span>
+                        ;
+                        <br />
+                      </span>
+                    ))}
+                  </code>
+                </pre>
+              </TabsContent>
+              <TabsContent value='code'>
+                <pre className='code-block p-4 rounded-lg bg-gray-100 text-primary overflow-auto max-h-[300px] text-sm'>
+                  <code>
+                    <span className='text-gray-500'>
+                      {'/* Tailwind Config - JSON */'}
+                      <br />
+                    </span>
+                    <p>
+                      {'{'}
+                      <br />
+                      {colors.map((color) => {
+                        return (
+                          <span key={color.id}>
+                            {"'"}
+                            {color.name.replace(/ /g, '-').toLowerCase()}
+                            {"'"}: {"'"}
+                            {color.hex}
+                            {"'"},
+                            <br />
+                          </span>
+                        );
+                      })}
+                      {'}'}
+                    </p>
+
+                    <span className='text-gray-500'>
+                      <br />
+                      {'/* CSV */'}
+                      <br />
+                    </span>
+                    {colors.map((color) => {
+                      return <span key={color.id}>{color.hex},</span>;
+                    })}
+                  </code>
+                </pre>
+              </TabsContent>
+            </Tabs>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant='outline' className='self-start'>
+              <PencilIcon className='size-4' />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className='flex flex-col'>
+            <DialogHeader>
+              <DialogTitle>Edit Your Color System</DialogTitle>
+            </DialogHeader>
+            <EditColorSystemForm
+              colorSystemId={colorSystem!.id}
+              initialName={colorSystem!.name}
+              onSuccess={handleUpdateSuccess}
+            />
+          </DialogContent>
+        </Dialog>
       </div>
 
-      <h1 className='text-2xl font-bold text-on'>{colorSystem.name}</h1>
+      <h1 className='text-2xl font-bold text-on'>🎨 {colorSystem!.name}</h1>
       <p>{colorSystem.description}</p>
       <div className='flex flex-col space-y-4'>
         <div>
@@ -190,7 +301,7 @@ const ColorSystemDisplay = ({ colorSystemId }: ColorSystemDisplayProps) => {
           <h2 className='text-xl font-bold mb-2'>Color Scales</h2>
           {colors.map((color, index) => (
             <div key={index} className='flex space-x-2'>
-              <ColorScale
+              <ColorScaleDisplay
                 baseColor={color}
                 bgColor={colors[5]}
                 colorSpace={selectedColorSpace}
